d045f37b30bbcdb7ae637da8e248a023293cf2e5
Riicky-hub <richardjosephace1960@gmail.com>
2023-03-30 08:58:36 -0300

    adicionando teste de segurança e paginação

--- product/tests/test_serializers/test_product_serializer.py
+++ product/tests/test_serializers/test_product_serializer.py
@@ -1,15 +1,20 @@
 from django.test import TestCase
 
 from product.factories import CategoryFactory, ProductFactory
-from product.serializers import CategorySerializer
+from product.serializers import ProductSerializer
 
 
-class TestCategorySerializer(TestCase):
+class TestProductSerializer(TestCase):
     def setUp(self) -> None:
-        self.category = CategoryFactory(title="food")
-        self.category_serializer = CategorySerializer(self.category)
+        self.category = CategoryFactory(title="technology")
+        self.product_1 = ProductFactory(
+            title="mouse", price=100, category=[self.category]
+        )
+        self.product_serializer = ProductSerializer(self.product_1)
 
-    def test_order_serializer(self):
-        serializer_data = self.category_serializer.data
-
-        self.assertEquals(serializer_data["title"], "food")
+    def test_product_serializer(self):
+        serializer_data = self.product_serializer.data
+        self.assertEquals(serializer_data["price"], 100)
+        self.assertEquals(serializer_data["title"], "mouse")
+        self.assertEquals(
+            serializer_data["category"][0]["title"], "technology")
